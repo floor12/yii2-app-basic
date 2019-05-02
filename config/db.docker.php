5ca8055c7bea817f0cc42649e6945012c0abf255
floor12 <floor12@floor12.net>
2019-05-02 11:51:01 +0200

    Настройка работы с докером в двух окружениях и запуск приемочных тестов  в докере.

---
+++ config/db.docker.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql;dbname=database',
+    'username' => 'yii2',
+    'password' => 'database',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
